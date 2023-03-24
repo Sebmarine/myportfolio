bd5e9d0df37f92785da523b17d4fafb72d2ce42c
Sebmarine <cbassnotw@gmail.com>
2023-03-24 15:35:03 -0700

    fixed overlapping issues

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="min-h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-20"
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
@@ -24,7 +24,7 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer "
+              className=" group text-white w-fit px-6 py-3 my-2 mx-auto sm:mx-0 text-center flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer "
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
